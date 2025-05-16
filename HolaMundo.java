c5771fdffac324caeb3b1e49c22d7818a42abdc2
ManuelLPzz <manuel.vivo.14@gmail.com>
2025-05-15 23:53:02 -0600

    Cambios dentro del submódulo MANUEL

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,6 +5,10 @@ public class HolaMundo {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
+
+        System.out.println("Hello marte!");
+
+        
         
     }
 }
